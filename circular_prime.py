b5ce1f39e9cd530067af1412284c434dee8e1de7
Vasanthreddy12345 <98875939+Vasanthreddy12345@users.noreply.github.com>
2022-10-30 19:53:19 +0530

    n=int(input()) m=n rev=0 while m>0:     r=m%10     rev=rev*10+r     m=m//10 is_prime=True for i in range(2,int(n**0.5)+1):     if n%i==0:         is_prime=False         break is_prime1=True for j in range(2,int(rev**0.5)+1):     if rev%j==0:         is_prime1=False         break if is_prime and is_prime1:     print('circular prime') elif is_prime:     print('prime but not a circular prime') else:     print('not prime')

--- circular_prime.py
+++ circular_prime.py
@@ -1,22 +1,23 @@
-def prime(x):
-    fc=0
-    for i in range(1,x+1):
-        if x%i==0:
-            fc=fc+1
-    if fc==2:
-        return 1
-    else:
-        return 0
 n=int(input())
 m=n
 rev=0
-while n>0:
-    r=n%10
+while m>0:
+    r=m%10
     rev=rev*10+r
-    n=n//10
-if prime(m)==1 and prime(rev)==1:
-    print("circular prime")
-elif prime(m)==1:
-    print("prime but not a circular prime")
+    m=m//10
+is_prime=True
+for i in range(2,int(n**0.5)+1):
+    if n%i==0:
+        is_prime=False
+        break
+is_prime1=True
+for j in range(2,int(rev**0.5)+1):
+    if rev%j==0:
+        is_prime1=False
+        break
+if is_prime and is_prime1:
+    print('circular prime')
+elif is_prime:
+    print('prime but not a circular prime')
 else:
-    print("not prime")
+    print('not prime')
